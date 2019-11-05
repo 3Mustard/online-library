1dffde05a363c03e5bf77546ca63e086e49c66a7
Mustard <48289475+3Mustard@users.noreply.github.com>
2019-11-05 12:59:25 -0500

    add redirect to logout method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < Sinatra::Base
 
+    #set's up the location of views and enables the use of sessions
     configure do 
         set :public_folder, 'public'
         set :views, 'app/views'
@@ -7,10 +8,12 @@ class ApplicationController < Sinatra::Base
         set :session_secret, 'library_secrets'
     end 
 
+    #set's the root directory to home.erb
     get '/' do 
         erb :home
     end 
 
+    #Helper methods!!
     helpers do 
         #returns true if a user is logged in
         def logged_in? 
@@ -19,10 +22,10 @@ class ApplicationController < Sinatra::Base
 
         #returns an instance of a user object whose email matches the session email set in login
         def current_user 
-            @current_user ||= User.find_by(:id => session[:user_id]) if session[:user_id] #if there is a session email then find the user by email and store it in instance variable
+            @current_user ||= User.find_by(:id => session[:user_id]) if session[:user_id] #if there is a session id then find the user who has the same id
         end 
         
-        #if a user object can be found by email and the password can be authenticated then the session email is set to that email.
+        #if a user object can be found by email and the password can be authenticated then the session id is set to the user's id
         def login(email, password)
             user = User.find_by(:email => email)
             if user && user.authenticate(password)
@@ -35,6 +38,7 @@ class ApplicationController < Sinatra::Base
         #clears the current session
         def logout
             session.clear 
+            redirect '/'
         end 
     end 
 end  
