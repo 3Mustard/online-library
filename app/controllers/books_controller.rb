832272211d651902199f63e99517f1a6607df5a7
Mustard <48289475+3Mustard@users.noreply.github.com>
2019-11-05 12:58:28 -0500

    fixed delete request route added confirm page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,55 +1,55 @@
-#helper methods located in application_controller.rb
+#helper methods logged_in? and current_user located in application_controller.rb
 
 class BooksController < ApplicationController
     
-    #render index of all the suer books
+    #if a user is logged in the index view of all the users books will be rendered
     get '/books/index' do
-      if !logged_in?
+      if !logged_in? 
         redirect '/login'
       else
-        @books = current_user.books.all #should grab all the books belonging to the current user but need to check. might not need .all
+        @books = current_user.books.all 
         erb :'/books/index'
       end 
     end
 
-    #render a new book form if the user is logged in
+    #if a user is logged in the new book form at books/new.erb is rendered
     get '/books/new' do
-        if !logged_in?
+        if !logged_in? 
             redirect '/login'
         else
-            erb :'/books/new'
+            erb :'/books/new' 
         end
     end
 
+    #handles a form submitted from books/new.erb
     post '/books/new' do
-        @book = Book.new(:title => params[:title], :author => params[:author]) #creates a new book object
-        @book.user = current_user #associates the book to the current user
+        @book = Book.new(:title => params[:title], :author => params[:author])
+        @book.user = current_user #associates the book to the current user 
         @book.save
         redirect "/books/#{@book.id}"
     end
 
+    #if the book belongs to the current user a page displaying the book will be rendered.
     get '/books/:id' do
-        @book = current_user.books.find_by_id(params[:id]) #finds the book by id
-        if @book.user == current_user #checks if the book entry belongs to the current user
+        @book = Book.find_by_id(params[:id])
+        if @book.user == current_user 
             erb :'/books/show'
         else
             "error"
         end
     end
 
-    #renders an edit form if the post belongs to the user and they are logged in
+    #renders an edit form if the book belongs to the current user
     get '/books/:id/edit' do
-        if !logged_in?
-            redirect '/login'
+        @book = Book.find_by_id(params[:id])
+        if @book.user == current_user
+            erb :'/books/edit'       
         else
-            if @book = current_user.books.find_by(params[:id])
-                erb :'/books/edit'
-            else
-                redirect '/books/index'
-            end
+            redirect '/books/index'
         end
     end
     
+    #handles a post from books/edit.erb by updating the values of a Book object
     patch '/books/:id' do
       @book = Book.find_by_id(params[:id])
       @book.title = params[:title]
@@ -57,8 +57,15 @@ class BooksController < ApplicationController
       @book.save
       redirect "/books/#{@book.id}"
     end
+
+    #sends user to a confirm delete page
+    get '/books/:id/delete' do
+        @book = Book.find_by_id(params[:id])
+        erb :'/books/delete'
+    end 
     
-    delete 'books/:id' do
+    #handles a delete request from books/delete.erb
+    delete '/books/:id' do
       @book = Book.find_by_id(params[:id])
       @book.delete
       redirect '/books/index'
