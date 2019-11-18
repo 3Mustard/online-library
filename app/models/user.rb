c412d1a2ba60ea373fce4160dc5231f7599b6f91
Mustard <48289475+3Mustard@users.noreply.github.com>
2019-11-18 10:35:59 -0500

    add validation for email uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
     has_secure_password #user needs a password to be saved
     validates :email, :presence => true #user objects cannot be saved without an email
+    validates :email, :uniqueness => true 
     has_many :books
 end 
